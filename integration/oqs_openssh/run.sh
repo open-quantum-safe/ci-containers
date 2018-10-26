22d3454db605961b6ccc1d5f7f81477b9386fda9
s24mishra <s24mishra@uwaterloo.ca>
2018-10-26 17:15:42 -0400

    Updates for Mac

--- integration/oqs_openssh/run.sh
+++ integration/oqs_openssh/run.sh
@@ -7,7 +7,7 @@ run_ssh_sshd() {
   for a in $3; do
   echo "    - KEX: $a"
   $BASEDIR/install/sbin/sshd -q -p 2222  -d -o "KexAlgorithms=$a" -f $BASEDIR/install/sshd_config -h $BASEDIR/install/ssh_host_ed25519_key >> $LOGS 2>&1 &
-  $BASEDIR/install/bin/ssh   -l $USER -p 2222 -o "KexAlgorithms="$a"" $HOST -F $BASEDIR/install/ssh_config -o StrictHostKeyChecking=no "exit" >> $LOGS 2>&1 
+  $BASEDIR/install/bin/ssh   -l ${USER} -p 2222 -o "KexAlgorithms="$a"" ${HOST} -F $BASEDIR/install/ssh_config -o StrictHostKeyChecking=no "exit" >> $LOGS 2>&1 
   A=`cat $LOGS| grep SSH_CONNECTION`
   if [ $? -eq 0 ];then
     echo "    - Result: SUCCESS"
@@ -19,10 +19,14 @@ run_ssh_sshd() {
 }
 
 build_openssl() {
-	echo "=============================="
-	echo "Building openssl"
-	cd "${BASEDIR}/openssl"
-	CFLAGS=-fPIC ./config shared --prefix=${BASEDIR}/install >> $1 2>&1 
+  echo "=============================="
+  echo "Building OpenSSL_1_0_2-stable"
+  cd "${BASEDIR}/openssl"
+  case "$OSTYPE" in
+    darwin*) env CFLAGS=-fPIC ./Configure shared darwin64-x86_64-cc --prefix=${BASEDIR}/install >> $1 2>&1 ;;
+    linux*) CFLAGS=-fPIC  ./Configure shared linux-x86_64 --prefix=${BASEDIR}/install >> $1 2>&1 ;;
+    *)        echo "Unknown operating system: $OSTYPE" ; exit 1 ;;
+  esac
 	make clean >> $1 2>&1 
 	make -j8 >> $1 2>&1 
 	make depend >> $1 2>&1 
@@ -60,7 +64,7 @@ build_openssh-portable() {
 	git clean -d -f -x >> $1 2>&1
 	git checkout -- . >> $1 2>&1
 	autoreconf -i >> $1 2>&1
-	./configure --prefix="${BASEDIR}/install" --enable-pq-kex --enable-hybrid-kex --with-ldflags="-Wl,-rpath -Wl,${BASEDIR}/install/lib" --with-libs=-lm --with-ssl-dir=${BASEDIR}/install/  --with-liboqs-dir="${BASEDIR}/install" --sysconfdir="${BASEDIR}/install"  >> $1 2>&1
+	./configure --prefix="${BASEDIR}/install" --enable-pq-kex --enable-hybrid-kex --with-ldflags="-Wl,-rpath -Wl,${BASEDIR}/install/lib" --with-libs=-lm --with-ssl-dir=${BASEDIR}/install/  --with-liboqs-dir="${BASEDIR}/install" --with-cflags=-I${BASEDIR}/install/include --sysconfdir="${BASEDIR}/install"  >> $1 2>&1
 	make clean >> $1 2>&1
 	make -j  >> $1 2>&1
 	make install >> $1 2>&1
@@ -96,7 +100,6 @@ cd tmp
 BASEDIR=`pwd`
 DATE=`date '+%Y-%m-%d-%H%M%S'`
 LOGS="${BASEDIR}/log-${DATE}.txt"
-USER=`who | awk '{print $1}'`
 HOST=`hostname`
 
 echo "To follow along with the testing process:"
@@ -126,12 +129,14 @@ echo "Cloning Openssh OQS master"
 if [ ! -d "${BASEDIR}/openssh-portable" ] ; then
     git clone --branch OQS-master https://github.com/open-quantum-safe/openssh-portable.git  >> $LOGS 2>&1
 fi
+<< 'EOF'
 
 rm -rf ${BASEDIR}/install
 build_openssl $LOGS
 build_liboqs_master $LOGS
 build_openssh-portable $LOGS
 generate_keys $LOGS
+EOF
 
 HKEX='ecdh-nistp384-bike1-L1-sha384@openquantumsafe.org ecdh-nistp384-bike1-L3-sha384@openquantumsafe.org ecdh-nistp384-bike1-L5-sha384@openquantumsafe.org ecdh-nistp384-frodo-640-aes-sha384@openquantumsafe.org ecdh-nistp384-frodo-976-aes-sha384@openquantumsafe.org ecdh-nistp384-sike-503-sha384@openquantumsafe.org ecdh-nistp384-sike-751-sha384@openquantumsafe.org ecdh-nistp384-oqsdefault-sha384@openquantumsafe.org'
 
@@ -144,6 +149,7 @@ echo "==========================================================================
 run_ssh_sshd "  SSH client and sever using hybrid key exchange methods" "  ======================================================" "$HKEX"
 run_ssh_sshd "  SSH client and sever using PQ only key exchange methods" "  =======================================================" "$PQKEX"
 
+
 rm -rf ${BASEDIR}/install
 build_openssl $LOGS
 build_liboqs_nist $LOGS
@@ -153,7 +159,6 @@ echo "Combination being tested: liboqs-nist, OpenSSL_1_0_2-stable, openssh-porta
 echo "============================================================================================="
 run_ssh_sshd "  SSH client and sever using hybrid key exchange methods" "  ======================================================" "$HKEX"
 run_ssh_sshd "  SSH client and sever using PQ only key exchange methods" "  =======================================================" "$PQKEX"
-
 
 
 
