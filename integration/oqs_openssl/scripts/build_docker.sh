c3a985a3957f25b8d22feb45606559eaea1347fd
Michael Baentsch <mib@zurich.ibm.com>
2019-10-29 08:52:18 +0100

    pwd corrected

--- integration/oqs_openssl/scripts/build_docker.sh
+++ integration/oqs_openssl/scripts/build_docker.sh
@@ -12,17 +12,16 @@ case "$OSTYPE" in
     linux*)   echo "Copying over required files from tmp/openssl..."  ;;
     *)        echo "Operating system: $OSTYPE not configured for dockerization" ; exit 1 ;;
 esac
-cd scripts
 # copy required files over:
 pwd
 ls -l
-cp tmp/openssl/apps/openssl dockerizer
-mkdir dockerizer/include
-mkdir dockerizer/lib
-cp tmp/openssl/libcrypto.a dockerizer/lib
-cp tmp/openssl/libssl.a dockerizer/lib
-cp tmp/openssl/oqs/lib/liboqs.a dockerizer/lib
-cp -R tmp/openssl/oqs/include/oqs dockerizer/include
-cp -R tmp/openssl/include/openssl dockerizer/include
+cp tmp/openssl/apps/openssl scripts/dockerizer
+mkdir scripts/dockerizer/include
+mkdir scripts/dockerizer/lib
+cp tmp/openssl/libcrypto.a scripts/dockerizer/lib
+cp tmp/openssl/libssl.a scripts/dockerizer/lib
+cp tmp/openssl/oqs/lib/liboqs.a scripts/dockerizer/lib
+cp -R tmp/openssl/oqs/include/oqs scripts/dockerizer/include
+cp -R tmp/openssl/include/openssl scripts/dockerizer/include
 
-cd dockerizer && docker build -t ubuntu-oqssl .
+cd scripts/dockerizer && docker build -t ubuntu-oqssl .
