c2991d6800c8a731c696eb3cac6b2494890db07d
Michael Baentsch <mib@zurich.ibm.com>
2019-10-29 15:54:04 +0100

    adapt local test to main test in open-quantum-safe/openssl

---
+++ integration/oqs_openssl/scripts/fixup_test_py.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Obtain openssl master testfile and change suitably:
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo $DIR
+cd /tmp
+wget https://raw.githubusercontent.com/open-quantum-safe/openssl/OQS-OpenSSL_1_1_1-stable/oqs_test/tests/test_openssl.py
+sed -e "s/os.path.join('..')/os.path.join('tmp','openssl')/g" test_openssl.py > $DIR/../tests/test_openssl.py-generated
+rm test_openssl.py
