6849289166deb530d4426edfee35729918d6fb5a
Michael Baentsch <mib@zurich.ibm.com>
2019-10-29 15:55:38 +0100

    fixup executed

--- integration/oqs_openssl/scripts/fixup_test_py.sh
+++ integration/oqs_openssl/scripts/fixup_test_py.sh
@@ -6,5 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo $DIR
 cd /tmp
 wget https://raw.githubusercontent.com/open-quantum-safe/openssl/OQS-OpenSSL_1_1_1-stable/oqs_test/tests/test_openssl.py
-sed -e "s/os.path.join('..')/os.path.join('tmp','openssl')/g" test_openssl.py > $DIR/../tests/test_openssl.py-generated
+sed -e "s/os.path.join('..')/os.path.join('tmp','openssl')/g" test_openssl.py > $DIR/../tests/test_openssl.py
 rm test_openssl.py
