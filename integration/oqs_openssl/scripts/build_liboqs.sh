130553f4923d8287672abf8dab705b0d65d46d72
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2019-12-30 11:01:32 +0100

    centos 7 added; circle-ci tests reduced

--- integration/oqs_openssl/scripts/build_liboqs.sh
+++ integration/oqs_openssl/scripts/build_liboqs.sh
@@ -7,6 +7,7 @@
 #  - LIBOQS: either master (default) or nist
 #  - OPENSSL_DIR: path to system OpenSSL installation; default /usr
 #  - PREFIX: path to install liboqs, default `pwd`/tmp/openssl/oqs
+#  - LINKTYPE: either static or dynamic (must be present)
 ###########
 
 set -exo pipefail
@@ -14,22 +15,23 @@ set -exo pipefail
 OPENSSL_DIR=${OPENSSL_DIR:-"/usr"}
 PREFIX=${OPENSSL_SRC_DIR:-"`pwd`/tmp/openssl/oqs"}
 
-if [ "x${LIBOQS}" == "xnist" ]; then
-    cd tmp/liboqs
-    if [ "x${CIRCLECI}" == "xtrue" ] || [ "x${TRAVIS}" == "xtrue" ]; then
-        make -j2
-    else
-        make -j
-    fi
-    make install-noshared PREFIX="${PREFIX}"
-else
-    cd tmp/liboqs
-    autoreconf -i
-    ./configure --prefix=${PREFIX} --enable-shared=no --enable-openssl --with-openssl-dir=${OPENSSL_DIR}
-    if [ "x${CIRCLECI}" == "xtrue" ] || [ "x${TRAVIS}" == "xtrue" ]; then
-        make -j2
-    else
-        make -j
-    fi
-    make install
+if [[ -z ${LINKTYPE} ]]; then
+   echo "LINKTYPE not set (static, dynamic). Exiting."
+   exit -1
 fi
+
+cd tmp/liboqs
+autoreconf -i
+
+if [ "$LINKTYPE" == "static" ]; then
+    ./configure --prefix=${PREFIX} --enable-shared=no --with-openssl=${OPENSSL_DIR}
+else
+    ./configure --prefix=${PREFIX} --enable-shared=yes --with-openssl=${OPENSSL_DIR}
+fi
+
+if [ "x${CIRCLECI}" == "xtrue" ] || [ "x${TRAVIS}" == "xtrue" ]; then
+    make -j2
+else
+    make -j
+fi
+make install
