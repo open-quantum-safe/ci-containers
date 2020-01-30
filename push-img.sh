a4c52e5646f2f7bd042baf25002f41fe693a3b88
xvzcf <xvzcf@users.noreply.github.com>
2020-01-30 14:44:19 -0500

    Refactored Dockerfiles and updated README.

--- push-img.sh
+++ push-img.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Push a specified image to
-# the Docker Hub.
+# Docker Hub.
 set -eu
 
 if [ "$#" -ne 1 ]; then
@@ -11,12 +11,13 @@ directory in this repository."
 fi
 
 BASENAME=$1
-LOCALIMAGENAME=oqs-"${BASENAME}"
+IMAGENAME=ci-"${BASENAME}"
 
 cd "${BASENAME}"
 
-docker build -t "${LOCALIMAGENAME}"
+docker build -t "${IMAGENAME}"
 
 printf "Pushing image to openqsafe."
-docker tag "${LOCALIMAGENAME}" openquantumsafe/ci-"${BASENAME}"
-docker push openquantumsafe/ci-"${BASENAME}"
+#TODO: Confirm tagging strategy
+docker tag "${IMAGENAME}" openquantumsafe/ci-"${IMAGENAME}":v1
+docker push openquantumsafe/ci-"${BASENAME}":v1
